6d1d2c7dd7035f0ce562fd5a7b00e5d08a95dd87
erik antony <erik.antony,mg@gmail.com>
2023-03-17 20:35:48 -0500

    eureka

--- microservicioautos/Carro-Service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ microservicioautos/Carro-Service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.config.client.ConfigServerInstanceProvider;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarroServiceApplication {
 
 
