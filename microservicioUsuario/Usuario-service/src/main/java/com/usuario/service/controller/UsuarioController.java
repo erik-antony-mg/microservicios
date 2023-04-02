12029561eab50adb90bed8ab34fb0e0eb3b90269
erik antony <erik.antony,mg@gmail.com>
2023-04-02 17:52:48 -0500

    cb

--- microservicioUsuario/Usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
+++ microservicioUsuario/Usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.usuario.service.controller;
 import com.usuario.service.entity.Usuario;
 import com.usuario.service.models.Carro;
 import com.usuario.service.service.UsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,7 @@ public class UsuarioController {
         return new ResponseEntity<>(usuarioService.guardarUsuario(usuario), HttpStatus.CREATED);
     }
 
+    @CircuitBreaker(name = "carroService", fallbackMethod = "fallbackGetCarro")
     @GetMapping("/{usuarioId}/carros")
     public ResponseEntity<List<Carro>> obtenerCarrosPorUsuario(@PathVariable Long usuarioId){
         List<Carro> carros=usuarioService.obtenerLosCarrosDelUsuario(usuarioId);
@@ -43,10 +45,18 @@ public class UsuarioController {
         return ResponseEntity.ok(carros);
     }
 
+    @CircuitBreaker(name = "carroService", fallbackMethod = "fallbackSaveCarro")
     @PostMapping("/{usuarioId}/crear/carro")
     public ResponseEntity<Carro> crearCarro(@PathVariable Long usuarioId,@RequestBody Carro carro){
         Carro nuevoCarro=usuarioService.crearCarro(carro,usuarioId);
         return ResponseEntity.ok(nuevoCarro);
     }
 
+    private   ResponseEntity<List<Carro>> fallbackGetCarro(@PathVariable Long usuarioId,RuntimeException e){
+    return new ResponseEntity("el usuario "+usuarioId+" tienen los carros en el taller",HttpStatus.OK);
+    }
+
+    private  ResponseEntity<Carro> fallbackSaveCarro(@PathVariable Long usuarioId,@RequestBody Carro carro, RuntimeException e){
+        return new ResponseEntity("el usuario "+usuarioId+" tienen los carros en el taller",HttpStatus.OK);
+    }
 }
